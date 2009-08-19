18f27d2b1cc5f66880c64affccd323cf11e88e4a
mihasya <m@mihasya.com>
2009-08-18 22:55:09 -0700

    graphing/formatting fixes, as well as minor cleanup

--- streamstats.py
+++ streamstats.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import sys, math
+import sys, math as m
 from collections import defaultdict
 from pprint import pprint
 
@@ -14,16 +14,17 @@ for line in sys.stdin.readlines():
 
 s = {} # store our stats in a dict for easy printings later
 s['count'] = len(distribution)
+s['total'] = sum(distribution.values())
 s['mean'] = float(total) / float(s['count'])
 s['maximum'] = max(distribution.values())
 s['minimum'] = min(distribution.values())
-s['stdev'] = math.sqrt(float(sum([math.pow(distribution[x]-s['mean'], 2) for x in distribution]))/float(s['count']))
+s['stdev'] = m.sqrt(float(sum([m.pow(distribution[x]-s['mean'], 2) for x in distribution]))/float(s['count']))
 s['outliers'] = 0
 
 token_len = max([len(x) for x in distribution.keys()]) + 1
 
 for x in distribution:
-    out = "%"+str(token_len)+"s %-60s %s %s\033[m"
+    out = "%"+str(token_len)+"s %-40s %s %s\033[m"
     outlier = (distribution[x] < s['mean']-s['stdev'] or s['mean']+s['stdev'] < distribution[x])
     if outlier:
         print "\033[0;31m",
@@ -31,7 +32,7 @@ for x in distribution:
     else:
         print "\033[0m",
     print out % ( str(x),
-        '|' * int(float(distribution[x])/s['count'] * 60),
+        '|' * int(m.ceil(float(distribution[x])/s['total'] * 40)),
         distribution[x],
         ['', '*'][outlier] )
 
