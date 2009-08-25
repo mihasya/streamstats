d80c8879d840625fecb356c6d1950a87f0df8765
mihasya <m@mihasya.com>
2009-08-25 08:36:39 -0700

    initial work on adding time

--- streamstats.py
+++ streamstats.py
@@ -15,13 +15,46 @@ parser = OptionParser()
 parser.add_option("-o", "--outliers", action="store_true", default=False,
     dest="outliers", help="Show only outliers")
 
+parser.add_option("-t", "--time", action="store_true", default=False,
+    dest="time", help="Use this command sequence to extract timestamps")
+
+parser.add_option("-p", "--pattern", action="store", default=None, metavar='PTRN',
+    dest="time", help="Use this command sequence to extract timestamps")
+
+"""
+parser.add_option("-s", "--start", action="store", default=None, metavar='TIME',
+    dest="start", help="Starting timestamp")
+
+parser.add_option("-e", "--end", action="store", default=None, metavar='TIME',
+    dest="end", help="Ending timestamp")
+"""
 (options, args) = parser.parse_args()
 
 distribution = {}
 total = 0
 
+print options
+
+if (not options['time'] and not options['pattern']):
+    pattern = None
+else:
+    if (options['pattern']):
+        pattern = options['pattern']
+    else:
+        pattern = "\[(?P<time>.+)\]\s+(?P<value>.+)\n";
+
+    import re
+    regex = re.compile(pattern)
+
 for line in sys.stdin.readlines():
-    value = line.strip('\n')
+    if (pattern):
+        groups = regex.match(line).groupdict()
+        value = groups['value']
+        if (options['time']):
+            time = groups['time']
+    else:
+        value = line.strip('\n')
+
     if value not in distribution:
         distribution[value] = 0
     distribution[value] += 1
